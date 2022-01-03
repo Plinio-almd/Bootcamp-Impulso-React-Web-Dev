189545e5aa0a7904890432f1d9a829a01b8a6085
Plinio-almd <plinio.pinda@gmail.com>
2022-01-03 14:45:00 -0300

    updating directory

---
+++ Práticas/Basecamp JavaScript/DOM/Módulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Plínio', 30));
+console.log(mostraCidade('Plínio', 'São José dos Campos'));
+console.log(mostraHobby('Plínio', 'Jogar Tetris'));
